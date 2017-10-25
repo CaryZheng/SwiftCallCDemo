0e51760af6bc65cfa3cbc8917ca04cba6dea3063
CaryZheng <CaryZhengMy@gmail.com>
2017-10-25 18:59:18 +0800

    Init project

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "MyCDemo",
+    dependencies: [
+    	.Package(url: "https://github.com/CaryZheng/CDemo.git", "1.0.0")
+  	]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,3 @@
+import MyLib
+
+MyLib().myTest()
